bd82ce3a2ceec09f28284d581cfdbd852ab98a67
Khalid ghlaib <khalidghalib426@gmail.com>
2026-02-14 22:32:08 +0430

    frontend set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+
+   //function 
+   
+
   return (
-    <div>App</div>
+    <div className='min-h-screen bg-gradient-to-br from-gray-50 to-gray-400
+    flex items-center  justify-center'>
+      <div className='bg-white rounded-2xl shadow-xl w-full max-w-lg p-8'>
+        
+                <h1 className='text-4xl font-bold text-grey-200 mb-6'>Task </h1>  
+       
+        <form  className=' flex items-center gap-2 shadow-md shadow-sm border border-gray-200 p-2 rounded-lg'>
+          <input className='flex-1 outline-none px-3 py-3 text-gray-700 placeholder-gray-400' type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} placeholder='what do you want to be done'
+          required/>
+          <button className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-medium  curser-pointer'>Add Task</button>
+        </form>
+      </div>
+      
+    </div>
   )
 }
 
