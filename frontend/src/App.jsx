4af720d6645c975bf03a0b4da150edd34f48290f
Khalid ghlaib <khalidghalib426@gmail.com>
2026-02-17 23:29:15 +0430

    setting up the db

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,9 +13,9 @@ function App() {
       const res  = await axios.post("/api/todos", {text: newTodo})
       setTodos([...todos,response.data])
       setNewTodo('');
-      
+
     } catch (error) {
-      
+     console.log("error adding todo:",error) 
     }
    }
 
@@ -26,10 +26,10 @@ function App() {
         
                 <h1 className='text-4xl font-bold text-grey-200 mb-6'>Task </h1>  
        
-        <form  className=' flex items-center gap-2 shadow-md shadow-sm border border-gray-200 p-2 rounded-lg'>
+        <form  onSubmit={addTodo} className=' flex items-center gap-2 shadow-md shadow-sm border border-gray-200 p-2 rounded-lg'>
           <input className='flex-1 outline-none px-3 py-3 text-gray-700 placeholder-gray-400' type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} placeholder='what do you want to be done'
           required/>
-          <button className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-medium  curser-pointer'>Add Task</button>
+          <button type='submit' className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-medium  curser-pointer'>Add Task</button>
         </form>
       </div>
       
