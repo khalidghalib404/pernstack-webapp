e26f328103da6486d17419ac2889581341fb469f
Khalid ghlaib <khalidghalib426@gmail.com>
2026-03-13 23:53:24 +0430

    update App.jsx and add install script

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 function App() {
   const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([]);
-   //function 
+   //function
    const addTodo = async (e) =>{
     e.preventDefualt();
     if (!newTodo.trim()) return;
@@ -18,13 +18,12 @@ function App() {
      console.log("error adding todo:",error) 
     }
    }
-
   return (
     <div className='min-h-screen bg-gradient-to-br from-gray-50 to-gray-400
     flex items-center  justify-center'>
       <div className='bg-white rounded-2xl shadow-xl w-full max-w-lg p-8'>
         
-                <h1 className='text-4xl font-bold text-grey-200 mb-6'>Task kjjkjjhgfhghgghjhgkjkhdskfjsdhfkjk</h1>  
+                <h1 className='text-4xl font-bold text-grey-200 mb-6'>Task kjjkjjhgfhghgghjhgkjkhdskfjsdhfkjk</h1>
        kjsdfh
         <form  onSubmit={addTodo} className=' flex items-center gap-2 shadow-md shadow-sm border border-gray-200 p-2 rounded-lg'>
           <input className='flex-1 outline-none px-3 py-3 text-gray-700 placeholder-gray-400' type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} placeholder='what do you want to be done'
@@ -36,5 +35,4 @@ function App() {
     </div>
   )
 }
-
 export default App
